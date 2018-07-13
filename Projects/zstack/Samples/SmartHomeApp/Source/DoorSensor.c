2bbd9da581df9d577e98ed2a8550747886ed23f4
Lianchun Liu <lianchun.liu@gmail.com>
2018-07-13 10:59:03 +0800

    m

--- Projects/zstack/Samples/SmartHomeApp/Source/DoorSensor.c
+++ Projects/zstack/Samples/SmartHomeApp/Source/DoorSensor.c
@@ -2,10 +2,16 @@
 #include "SmartHomeApp.h"
 #include "hal_key.h"
 
+#define LIGHT0_PIN P1_0
+#define LIGHT1_PIN P1_1
+#define LIGHT2_PIN P1_4
+
 
 void DoorSensor_Init()
 {
-  
+  LIGHT0_PIN = 1;
+  LIGHT1_PIN = 1;
+  LIGHT2_PIN = 1;
 }
 
 void DoorSensor_Update()
@@ -22,7 +28,7 @@ void DoorSensor_HandleKeys(uint8 keys)
   }
   
   if (keys & HAL_KEY_SW_5) {
-    SmartHome_SendCmd("TurnOffLater DoorLight 1 30");
+    SmartHome_SendCmd("TurnOffLater DoorLight 1 60");
   }
 }
 
